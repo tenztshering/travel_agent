de0bd2798ae6cc42e85eda3aad5b4f22b48edb77
Tenzin Tshering <89956683+tenztshering@users.noreply.github.com>
2023-01-05 14:09:29 +0600

    add changes

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles';
 
-const NewFeatures = ({ imgUrl, title, subtitle, link }) => (
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
   <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
     <div
       className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
